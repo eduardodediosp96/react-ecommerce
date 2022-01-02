a7fb540132b83679b17bfd628a6d681ed643e51e
unknown <daniela.argumanis@gmail.com>
2022-01-02 12:17:24 -0500

    Next app created

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
-/** @type {import('next').NextConfig} */
-module.exports = {
+/* eslint-disable @typescript-eslint/no-var-requires */
+const withLess = require('next-with-less')
+
+module.exports = withLess({
   reactStrictMode: true,
-}
+  lessLoaderOptions: {},
+})
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import '../styles/antd.less'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
