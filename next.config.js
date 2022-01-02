9db2fbd54f775bbe164d5c2405981a3b3add4337
Eduardo Dedios <eduardodediosp96@gmail.com>
2022-01-02 11:04:14 -0500

    adding antd

--- next.config.js
+++ next.config.js
@@ -1,4 +1,32 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
-  reactStrictMode: true,
-}
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withAntdLess = require('next-plugin-antd-less');
+
+module.exports = withAntdLess({
+  modifyVars: { '@primary-color': '#04f' }, // optional
+  lessVarsFilePath: './src/styles/variables.less', // optional 
+  lessVarsFilePathAppendToEndOfContent: false, // optional
+  // optional https://github.com/webpack-contrib/css-loader#object
+  cssLoaderOptions: {
+    // ... 
+    mode: "local",
+    localIdentName: "[path][name]__[local]--[hash:base64:5]", // invalid! for Unify getLocalIdent (Next.js / CRA), Cannot set it, but you can rewritten getLocalIdentFn
+    exportLocalsConvention: "camelCase",
+    exportOnlyLocals: false,
+    // ...
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    getLocalIdent: (context, localIdentName, localName, options) => {
+      return "whatever_random_class_name";
+    },
+  },
+  // Other Config Here...
+
+  webpack(config) {
+    return config;
+  },
+
+  // ONLY for Next.js 10, if you use Next.js 11, delete this block
+  future: {
+    webpack5: true,
+  },
+});
