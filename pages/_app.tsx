b5c34ad5ebd42ff26bd2734fdd34569f026ff78e
Eduardo Dedios <eduardodediosp96@gmail.com>
2022-01-02 01:38:45 -0500

    firstcommit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,7 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+export var APP_VERSION = 'ASDASD'
+export var APP_VERSION_2 = 'ASDASD'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
